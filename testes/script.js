aa73e847ba676b91f452e2d55cf1e0a806fee029
Guilherme Ferreira <guilhermefeirense@gmail.com>
2025-09-18 16:52:28 +0100

    testes

---
+++ testes/script.js
@@ -0,0 +1,11 @@
+var n1 = Number(window.prompt('Digite um numero'))
+var n2 = Number(window.prompt('Digite outro numero'))
+var s = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`)
+
+
+var nome = window.prompt('Qual é o teu nome?')
+document.write(`Teu nome é ${nome} <br>`) 
+document.write(`Teu nome tem ${nome.length} <br>`) 
+document.write(`Teu nome em letras maiúsculas é ${nome.toUpperCase()} <br>`) 
+document.write(`Teu nome em letras minúsculas é ${nome.toLowerCase()} <br>`)
