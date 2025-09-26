8858cbe54a5224b8e900fd7054a6cc57409dbe24
Guilherme Ferreira <guilhermefeirense@gmail.com>
2025-09-26 16:16:42 +0100

    atualziei o teste02

--- testes02/script.js
+++ testes02/script.js
@@ -1,31 +1,56 @@
+
+//Variavel de lista para tarefas
+let tarefas =[]
+
 function adicionarTarefa() {
 
-    //recebe valor do input do usuário
+    //recebe valor do input do usuario
     const inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value.trim()
 
     //variavel para a mensagem depois de Erro ou Sucesso
     const mensagem = document.getElementById ("mensagem")
 
-    //se o valor do input for vazio, mostre uma mensagem de erro
+    //Se o valor do input for vazio, mostre uma mensagem e Erro
     if (tarefa == ""){
-        //mostre uma mensagem de erro
+
+        //mostre uma mensagem de Erro
         let mensagemErro = "Digite uma tarefa para adicioná-la a a sua lista!"
         mensagem.textContent = mensagemErro
     } 
     else {
-        //mensagem de tarefa adicionada com sucesso
+
+        //mostre uma mensagem de Sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
-        mensagem.textContent = mensagemSucesso 
+        mensagem.textContent = mensagemSucesso
 
-        //cria novo item (li) e insere na (lista ul)
-        const listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
-        novaTarefa.textContent = tarefa
-        listaTarefas.appendChild(novaTarefa)
+        //variavel de lista (tarefas) adiciona sempre o valor do input do usuario (tarefa)
+        tarefas.push(tarefa)
+
+        renderizarTarefas()
     }
-    
-    //limpa o input do usuário
-    inputTarefa.value = ""
 
+    //limpa o input do usuario
+    inputTarefa.value = ""
+}
+
+function renderizarTarefas(){
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+
+    //for itens na lista 
+    //1. item inicial (iterador)
+    //2. item final (condição)
+    //3. se vai de 1 em 1 elemento ou se pula
+
+    // for (iterador, condição, frequencia)
+    
+    let i = 0
+    for (i; i < tarefas.length; i++){
+
+        //cria um novo item (li) e insere na lista (ul)
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefas [i]
+        listaTarefas.appendChild(novaTarefa)
+    } 
 }
