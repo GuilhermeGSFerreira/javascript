1e603ad966e96a66c96d94a680b4e1c0769cc916
Guilherme Ferreira <guilhermefeirense@gmail.com>
2025-09-26 23:33:33 +0100

    js

--- testes02/script.js
+++ testes02/script.js
@@ -47,12 +47,63 @@ function renderizarTarefas(){
 
     // for (iterador, condição, frequencia)
     
-    let i = 0
-    for (i; i < tarefas.length; i++){
+    for (let i = 0; i < tarefas.length; i++){
 
         //cria um novo item (li) e insere na lista (ul)
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas [i]
+
+        //criei botao remover tarefa
+        let botaoremover = document.createElement("button")
+        botaoremover.className = "remover"
+        botaoremover.textContent = "remover"
+        botaoremover.onclick = () => removerTarefa(i)
+    
+        //criei botao editar tarefa
+        let botaoeditar = document.createElement("button")
+        botaoeditar.className = "editar"
+        botaoeditar.textContent = "editar"
+        botaoeditar.onclick = () => editarTarefa(i)
+
+        // botao sao filhos das (li)
+        novaTarefa.appendChild(botaoremover)
+        novaTarefa.appendChild(botaoeditar)
+
+        //(li) filho de (ul)
         listaTarefas.appendChild(novaTarefa)
     } 
+}
+
+function removerTarefa(i){
+    
+    //remover 1
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i){
+    
+    //criar um prompt para o que queres dizer e editar na lista (nome da tarefa)
+    let tarefaEditada = prompt("Edite a Tarefa:")
+    if (tarefaEditada.trim() !== ""){
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    }  
+}
+
+function limparLista(){
+
+    //caso nao haja Tarefas para limpar, ao clicar no botao (limpar tudo) aparece as mensagem de (Nada) ou (mensagem)
+    if (tarefas == ""){
+        const Nada = document.getElementById ("mensagem")
+        Nada.textContent = "Não tem nada para Limpar"
+    }
+    else{
+        tarefas.length = 0
+
+        const mensagem = document.getElementById ("mensagem")
+        mensagem.textContent = "Lista de Tarefas Limpa com Sucesso"
+    }
+
+    renderizarTarefas()
 }
