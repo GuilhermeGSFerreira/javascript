31f9b863db8dd64c2de52a93c4b698e02853e775
Guilherme Ferreira <guilhermefeirense@gmail.com>
2025-09-27 18:47:17 +0100

    Update script.js

--- exercicios/exercicio7/script.js
+++ exercicios/exercicio7/script.js
@@ -1,12 +1,12 @@
 // Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados. Exemplos de operadores de comparação: >, <, >=, <= e outros.
 
 
-let numX = 1;
-let numY = 2;
-let numZ = 3;
+let numX = 1
+let numY = 2
+let numZ = 3
 
-console.log('NumX e maior que numY? ' + (numX > numY));
-console.log('NumX e menor que numY? ' + (numX < numY));
-console.log('NumX e maior ou igual a numZ? ' + (numX >= numZ));
-console.log('NumX e menor ou igual a numZ? ' + (numX <= numZ));
-console.log('NumZ e igual a numY? ' + (numZ == numY));
+console.log('NumX e maior que numY? ' + (numX > numY))
+console.log('NumX e menor que numY? ' + (numX < numY))
+console.log('NumX e maior ou igual a numZ? ' + (numX >= numZ))
+console.log('NumX e menor ou igual a numZ? ' + (numX <= numZ))
+console.log('NumZ e igual a numY? ' + (numZ == numY))
